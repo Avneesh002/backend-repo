b2347ff9be086d184055f4392b7732fc0befaacd
Avneesh002 <Avneeshgrover8@gmail.com>
2023-02-18 19:47:23 +0530

    sfaf

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.use(express.json());
 app.use(cors());
 
 const { connection } = require("./configs/db");
-const { userRouter } = require("./user.router");
-const { todoRouter } = require("./todo.Router");
+const { userRouter } = require("./user.router.js");
+const { todoRouter } = require("./todo.Router.js");
 const { authentication } = require("./Middleware/authentication.middleware.");
 
 app.get("/", (req, res) => {
