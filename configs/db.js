6f39056bfdede8a6dc2c247d4fca9b86429979e5
Avneesh002 <Avneeshgrover8@gmail.com>
2023-02-18 16:08:43 +0530

    uncommenting again

--- configs/db.js
+++ configs/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-
+mongoose.set("strictQuery", true);
 const connection = mongoose.connect(process.env.SERVER_LINK);
 
 module.exports = { connection };
