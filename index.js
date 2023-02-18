5e6b07226a3cc089eccf1b7b2517c4def68cf910
Avneesh002 <Avneeshgrover8@gmail.com>
2023-02-18 16:01:34 +0530

    testing

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
-require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
 
 const { connection } = require("./configs/db");
-const { userRouter } = require("./Routers/user.router");
-const { todoRouter } = require("./Routers/todo.router");
+// const { userRouter } = require("./Routers/user.router");
+// const { todoRouter } = require("./Routers/todo.router");
 const { authentication } = require("./Middleware/authentication.middleware.");
 
 app.get("/", (req, res) => {
   res.send("THIS IS YOUR HOMEPAGE");
 });
 
-app.use("/users", userRouter);
+// app.use("/users", userRouter);
 
 app.use(authentication);
-app.use("/todos", todoRouter);
+// app.use("/todos", todoRouter);
 
 app.listen(process.env.PORT, async () => {
   try {
